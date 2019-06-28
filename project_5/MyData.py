95d14bfb2411fbb705a843d62bb8a6c1f1a98db5
chinasilva <www476369545@qq.com>
2019-06-28 09:54:45 +0800

    更新处理图片方法

--- project_5/MyData.py
+++ project_5/MyData.py
@@ -4,6 +4,9 @@ import numpy as np
 from torch.utils import data
 from PIL import Image
 from ProcessImage import ProcessImage
+from datetime import datetime
+
+
 class MyData(data.Dataset):
     def __init__(self,path):
         super().__init__()
@@ -11,11 +14,12 @@ class MyData(data.Dataset):
         self.dataset=[]
         # 以列表形式链接所有地址
         self.dataset.extend(os.listdir(path))
-        tagPath=r"C:/Users/liev/Desktop/dataset/celeba/Anno/list_bbox_celeba.txt"
+        self.tagPath=r"C:/Users/liev/Desktop/dataset/celeba/Anno/list_bbox_celeba.txt"
         tagWritePath=r"C:\Users\liev\Desktop\code\deeplearning_homework\project_5\pic\12\negative\12_list_bbox_celeba.txt"
         saveImgPath=r'C:\Users\liev\Desktop\code\deeplearning_homework\project_5\pic\12\negative'
         #处理图片
-        self.process=ProcessImage(tagPath,tagWritePath,saveImgPath)
+        self.tagLst=[]
+        self.process=ProcessImage(self.tagPath,tagWritePath,saveImgPath,self.tagLst)
     
     # 获取数据长度
     def __len__(self):
@@ -24,16 +28,22 @@ class MyData(data.Dataset):
     # 获取数据中x,y
     def __getitem__(self, index):
         imgName=self.dataset[index]
-        self.process.processImage(self.path,imgName)
-        return imgName
+        imageInfo,tagLst= self.process.processImage(self.path,imgName)
+        return imgName,tagLst
 
 
 if __name__ == "__main__":
     imagePath=r'C:/Users/liev/Desktop/dataset/celeba/img_celeba/'
     myData=MyData(imagePath)
-    trainData=data.DataLoader(myData,batch_size=1,shuffle=True)
-    for i in enumerate(trainData):
+    trainData=data.DataLoader(myData,batch_size=5,shuffle=False)
+    for i,(imgName,tagLst) in enumerate(trainData):
         print(i)
+        # a=datetime.now()
+        # b=datetime.now()
+        # print("第{}轮次,耗时{}ms".format(i,(b-a).microseconds//1000))
+
+        # myData.process.tagWritePath(myData.tagPath,tagLst)
+
     
     
 
--- project_5/ProcessImage.py
+++ project_5/ProcessImage.py
@@ -3,14 +3,14 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-
 class ProcessImage():
-    def __init__(self,tagPath,tagWritePath,saveImgPath):
+    def __init__(self,tagPath,tagWritePath,saveImgPath,newTagLst):
         self.tagPath=tagPath
         self.tagWritePath=tagWritePath
         self.saveImgPath=saveImgPath
+        self.newTagLst=newTagLst
 
-    def processImage(self,imagePath,imgName,outImgSize=300):
+    def processImage(self,imagePath,imgName,outImgSize=12):
         '''
         逐张处理图片
         outImgSize:默认处理尺寸为12
@@ -25,13 +25,12 @@ class ProcessImage():
                         imageInfo = line.split()#将单个数据分隔开存好
                         #resize 图片 
                         targetSize=(outImgSize,outImgSize)
-                        newImgName=self.padImage(img,imgName, targetSize,self.saveImgPath)
-                        offset=self.changeTag(img,imageInfo,targetSize)
-                        # self.writeTag(self.tagWritePath,newImgName,offset=offset)
-                        
-                        self.myRectangle(imageInfo,img,imgName)
-
-                        return imageInfo
+                        offset=self.padImage2(img,imgName,imageInfo, targetSize,self.saveImgPath)
+                        self.newTagLst.append(offset)
+                        if len(self.newTagLst)%100==0: # 每1000次写一次
+                            self.writeTag(self.tagWritePath,self.newTagLst)
+                            self.newTagLst=[]
+                        return imageInfo,self.newTagLst
         except:
             print("ERROR:",imgName)
 
@@ -43,14 +42,15 @@ class ProcessImage():
             data = f.readlines()  #txt中所有字符串读入data
         return data
 
-    def writeTag(self,path,imgName,offset):
+    def writeTag(self,path,tagLst):
         '''
         写标签文件
         '''
-        x1,x2,y1,y2=offset
         with open(path, 'a+') as f:
-            f.write(str.format("{0}  {1}  {2}  {3}  {4}",imgName,x1,x2,y1,y2))
-            f.write('\n')
+            for i,tag in enumerate(tagLst):
+                newImgName,x1,x2,y1,y2=tag
+                f.write(str.format("{0}  {1}  {2}  {3}  {4}",newImgName,x1,x2,y1,y2))
+                f.write('\n')
 
     def myRectangle(self,imageInfo,img,imgName):
         '''
@@ -71,7 +71,7 @@ class ProcessImage():
 
     def padImage(self, image,imgName, targetSize,savePath):
         '''
-        按比例缩放并填充
+        按比例缩放并填充,先填充后缩放
         '''
         iw, ih = image.size  # 原始图像的尺寸
         w, h = targetSize  # 目标图像的尺寸
@@ -90,38 +90,38 @@ class ProcessImage():
 
         return newImgName
 
-    def changeTag(self,image,imageInfo,targetSize):
+    def padImage2(self, image,imgName,imageInfo, targetSize,savePath):
         '''
-        更改标签,将标签改成偏移量形式
+        1.按比例缩放并填充，先缩放后填充
+        2.进行记录标签要更改的偏移量
         '''
-        iw, ih = image.size  # 原始图像的尺寸
-        w, h = targetSize  # 目标图像的尺寸
-        scale = max(iw / w, ih / h)  # 转换的最大比例倍数
-        
-        minValue= min(iw, ih)
-        padding=(iw-minValue)/2
-        
-        offsetX1=(targetSize-padding)
-        offsetY1=0
-
         x1=float(imageInfo[1]) 
         y1=float(imageInfo[2])
         width=float(imageInfo[3])
         height=float(imageInfo[4])
-        x2=x1+width
-        y2=y1+height
-        newX1=x1/scale
-        newY1=y1/scale
-        newX2=x2/scale
-        newY2=y2/scale
-        offsetX1=x1-newX1
-        offsetY1=y1-newY1
-        offsetX2=x2-newX2
-        offsetY2=y2-newY2
-        # offset=(offsetX1,offsetY1,offsetY2,offsetY2)
-        offset=(newX1,newY1,width,height)
 
+        iw, ih = image.size  # 原始图像的尺寸
+        w, h = targetSize  # 目标图像的尺寸
+        maxValue= max(iw, ih)
+        paddingW=(maxValue-iw)//2
+        paddingH=(maxValue-ih)//2
+        # 先填充,后resize
+        newImage = Image.new('RGB', (maxValue,maxValue), (255,255,255))
+        newImage.paste(image, (paddingW,paddingH))  # 将图像填充为中间图像，两侧为灰色的样式
+        
+        #求出偏移量，并且对偏移量进行放缩，默认目标图w=h
+        offsetX1= round( (x1+paddingW)/w, 2)
+        offsetY1= round((y1+paddingH)/w,2)
+        offsetX2= round(((x1+width)-iw+paddingW)/w,2)
+        offsetY2= round(((y1+height)-ih+paddingH)/w,2)
+
+        #进行缩放
+        newImage = newImage.resize((w, h), Image.BICUBIC)  # 缩小图像
+        newImgName=os.path.join(savePath,'12new-'+imgName)
+        newImage.save(newImgName)
+        offset=(imgName,offsetX1,offsetY1,offsetX2,offsetY2)
         return offset
+
 
 # if __name__ == "__main__":
 #     tagPath=r"C:/Users/liev/Desktop/dataset/celeba/Anno/list_bbox_celeba.txt"
