b2b7328bb8aefa3bd47a0b3b71b19dc6979f61eb
chinasilva <www476369545@qq.com>
2019-07-03 22:45:24 +0800

    完成测试网络

--- project_5/src/Untitled-1.py
+++ project_5/src/Untitled-1.py
@@ -4,14 +4,32 @@
 # 3.在进行GPU训练时需要对输入，网络(初始化)，输出等进行数据传入GPU
 
 import torch
-from MyNet import PNet,RNet,ONet
+import numpy as np
 
+from MyNet import PNet,RNet,ONet
+from utils import nms,createImage,pltFun,deviceFun
 test= torch.Tensor(3,3,100,100)
-# a= torch.range(1,3)
-net=PNet()
-a,b,c=net(test)
-a=a.reshape(3,1,1,1)
-# a=a[:,:,:,0].reshape(-1,1)
-a=a.reshape(-1,1) #.squeeze(2).squeeze(2)
-print(a)
-print(a.size())
+test2= torch.Tensor(3,3,100,100)
+lst=[test,test2,test2]
+print(torch.stack(lst,dim=4).size())
+
+# a= torch.range(1,8)
+# a=a.reshape(2,1,2,2)
+# b=a[:,:1]
+# print("---",b) 
+# # net=PNet()
+# # a,b,c=net(test)
+# # a=a.reshape(3,1,1,1)
+# # # a=a[:,:,:,0].reshape(-1,1)
+# # a=a.reshape(-1,1) #.squeeze(2).squeeze(2)
+# print(a)
+# print(a.size())
+
+
+# PLst=np.array([
+# (188,188,201,198,0.9496198892593384),
+# (189,189,201,198,0.9496198892593384)
+# ])
+
+# a=PLst[nms(PLst,overlap_threshold=0.7,mode='union')]
+# print(a[0][0])
