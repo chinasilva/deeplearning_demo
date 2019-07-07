cd4b0c5794f78e9a73e29660ca66b2f57507e341
chinasilva <www476369545@qq.com>
2019-07-07 22:15:58 +0800

    更新linux地址

--- project_5/src/TrainONet.py
+++ project_5/src/TrainONet.py
@@ -1,7 +1,7 @@
 from MyTrain import MyTrain
 
 if __name__ == "__main__":
-    imgPath=r'D:\my_celebea\pic\48'
-    tagPath=r'D:\my_celebea\txt\48list_bbox_celeba.txt'
+    imgPath=r'/mnt/D/my_celebea/pic/48'
+    tagPath=r'/mnt/D/my_celebea/txt/48list_bbox_celeba.txt'
     myTrain=MyTrain(Net='ONet',epoch=100,batchSize=512,imgPath=imgPath,tagPath=tagPath)
     myTrain.train()
